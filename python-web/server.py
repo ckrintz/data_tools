2f0e0d21729df4789db69d80d236f5e740830af8
Chandra <ckrintz@ucsb.edu>
2019-11-06 12:15:15 -0800

    ported to python3

--- python-web/server.py
+++ python-web/server.py
@@ -1,10 +1,33 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from socketserver import ThreadingMixIn
-from http.server import SimpleHTTPRequestHandler, HTTPServer
-import os, sys, cgi, time
-import urllib.parse
-#https://pymotw.com/2/BaseHTTPServer/index.html#module-BaseHTTPServer
-class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
-    
+import threading, cgi
+
+#test using curl and HTTP via: (USE_HTTPS=False)
+#GET: curl http://localhost:8000
+
+#test using curl and HTTP via: (USE_HTTPS=False)
+#POST: curl -d name=admin -d shoesize=12 http://localhost:8000
+
+#test using curl and HTTP:  (USE_HTTPS=False)
+#put the following 2 lines without the # in a file test.txt
+	#hello=world;
+	#hey=you
+#via POST and a file: 
+#curl -d @test.txt http://localhost:8000
+
+#test using curl and https (set USE_HTTPS below to True), use -k to allow self-signed
+#generate a self-signed certificate on server side:
+#openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
+#POST: curl -k -d name=admin -d shoesize=12 https://localhost:8000
+USE_HTTPS = False
+
+class Handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello world\t' + threading.currentThread().getName().encode() + b'\t' + str(threading.active_count()).encode() + b'\n')
+
     def do_POST(self):
         print('post')
         # Parse the form data posted
@@ -18,76 +41,48 @@ class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
         # Begin the response
         self.send_response(200)
         self.end_headers()
-        self.wfile.write('Client: %s\n' % str(self.client_address))
-        self.wfile.write('User-agent: %s\n' % str(self.headers['user-agent']))
-        self.wfile.write('Path: %s\n' % self.path)
-        self.wfile.write('Form data:\n')
+        self.wfile.write(('Client: %s\n' % str(self.client_address)).encode())
+        self.wfile.write(('User-agent: %s\n' % self.headers['user-agent']).encode())
+        self.wfile.write(('Path: %s\n' % self.path).encode())
+        self.wfile.write(('Form data:\n').encode())
 
         # Echo back information about what was posted in the form
+        print(form.keys())
         for field in form.keys():
             field_item = form[field]
+            print(field_item)
             if field_item.filename:
                 # The field contains an uploaded file
+                print('file')
                 file_data = field_item.file.read()
                 file_len = len(file_data)
                 del file_data
-                self.wfile.write('\tUploaded %s as "%s" (%d bytes)\n' % \
-                        (field, field_item.filename, file_len))
+                self.wfile.write(('\tUploaded %s as "%s" (%d bytes)\n' % \
+                        (field, field_item.filename, file_len)).encode())
             else:
                 # Regular form value
                 value = form[field].value
-                self.wfile.write('\t%s=%s\n' % (field, value))
+                self.wfile.write(('\t%s=%s\n' % (field, value)).encode())
                 if value == 'on' or value == 'off':
                     myts = time.time()
                     print('{0}:{1}:{2}'.format(myts,field,value))
                     sys.stdout.flush()	
         return
 
-    def do_GET(self):
-        print('get')
-        parsed_path = urlparse.urlparse(self.path)
-        message_parts = [
-                'CLIENT VALUES:',
-                'client_address=%s (%s)' % (self.client_address,
-                                            self.address_string()),
-                'command=%s' % self.command,
-                'path=%s' % self.path,
-                'real path=%s' % parsed_path.path,
-                'query=%s' % parsed_path.query,
-                'request_version=%s' % self.request_version,
-                '',
-                'SERVER VALUES:',
-                'server_version=%s' % self.server_version,
-                'sys_version=%s' % self.sys_version,
-                'protocol_version=%s' % self.protocol_version,
-                '',
-                'HEADERS RECEIVED:',
-                ]
-        for name, value in sorted(self.headers.items()):
-            message_parts.append('%s=%s' % (name, value.rstrip()))
-        message_parts.append('')
-        message = '\r\n'.join(message_parts)
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(message)
-        return
+
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+    pass
+
+def run():
+    server = ThreadingSimpleServer(('0.0.0.0', 8000), Handler)
+    if USE_HTTPS:
+        import ssl
+        server.socket = ssl.wrap_socket(server.socket, keyfile='./key.pem', certfile='./cert.pem', server_side=True)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("\nShutting down server per users request.")
 
 
 if __name__ == '__main__':
-
-    IP = 'localhost'
-    PORT = 8000 
-    if len(sys.argv) > 2:
-        PORT = int(sys.argv[2])
-        IP = sys.argv[1]
-    elif len(sys.argv) > 1:
-        PORT = int(sys.argv[1])
-    CWD = os.getcwd()
-    server = ThreadingSimpleServer((IP, PORT), SimpleHTTPRequestHandler)
-    print("Serving HTTP traffic from", CWD, "on", IP, "using port", PORT)
-    try:
-        while 1:
-            sys.stdout.flush()
-            server.handle_request()
-    except KeyboardInterrupt:
-        print("\nShutting down server per users request.")
+    run()
